d3be9fa2c635b9a062b4585ab91af0e862a9c2d4
nick hansen <n-hansen@users.noreply.github.com>
2019-07-09 21:00:20 -0700

    dont use the cli

--- src/LFrac.hs
+++ src/LFrac.hs
@@ -1,4 +1,4 @@
-import Diagrams.Backend.Cairo.CmdLine
+import Diagrams.Backend.Cairo
 
 ell :: Diagram B
 ell = fromOffsets [ unitY
@@ -10,7 +10,24 @@ ell = fromOffsets [ unitY
                   ]
       # strokeLoop
       # centerXY
-      # fc white
 
-main = mainWith
-       ell
+lv2 = mconcat [ ell
+                # fc red
+              , ell
+                # rotateBy 0.75
+                # translate (r2 (-1, -1))
+                # fc purple
+              , ell
+                # translate (r2 (1,-1))
+                # fc blue
+              , ell
+                # rotateBy 0.25
+                # translate (r2 (1,1))
+                # fc green
+              ]
+
+main = renderCairo out sz d
+  where
+    out = "out/lfrac.png"
+    sz = mkSizeSpec2D (Just 400) Nothing
+    d = lv2
