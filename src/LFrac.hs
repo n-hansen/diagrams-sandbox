83c20d8b4b0d4e4ee226a96a51a51ebba8d1bf4f
nick hansen <n-hansen@users.noreply.github.com>
2019-07-09 22:49:50 -0700

    made some progress

--- src/LFrac.hs
+++ src/LFrac.hs
@@ -1,33 +1,38 @@
-import Diagrams.Backend.Cairo
+import           Diagrams.Backend.Cairo
 
 ell :: Diagram B
-ell = fromOffsets [ unitY
-                  , unitX
-                  , 2 *^ unit_Y
-                  , 2 *^ unit_X
-                  , unitY
-                  , unitX
-                  ]
-      # strokeLoop
-      # centerXY
-
-lv2 = mconcat [ ell
-                # fc red
-              , ell
-                # rotateBy 0.75
-                # translate (r2 (-1, -1))
-                # fc purple
-              , ell
-                # translate (r2 (1,-1))
-                # fc blue
-              , ell
-                # rotateBy 0.25
-                # translate (r2 (1,1))
-                # fc green
+ell =
+  fromOffsets [ unitY
+              , unitX
+              , 2 *^ unit_Y
+              , 2 *^ unit_X
+              , unitY
+              , unitX
               ]
+  # strokeLoop
+  # centerXY
+  # fc red
+
+layTiles tile =
+  let tile' tr rot o =
+        opacity o
+        . translate tr
+        . rotateBy rot
+        . scale 0.5
+        $ tile
+      t1 = tile' zero 0 1
+      t2 = tile' (r2 (negate xShift, negate yShift)) 0.75 0.8
+      t3 = tile' (r2 (xShift, negate yShift)) 0 0.8
+      t4 = tile' (r2 (xShift, yShift)) 0.25 0.8
+  in
+    mconcat [t1,t2,t3,t4]
+
+  where
+    xShift = 0.5
+    yShift = 0.5
 
 main = renderCairo out sz d
   where
     out = "out/lfrac.png"
     sz = mkSizeSpec2D (Just 400) Nothing
-    d = lv2
+    d = layTiles . layTiles . layTiles $ ell
