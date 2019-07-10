277475403fbf06dbadb7cf2a60a271fc6b3df0fd
nick hansen <n-hansen@users.noreply.github.com>
2019-07-09 20:08:41 -0700

    set up custom prelude

---
+++ lib/Prelude.hs
@@ -0,0 +1,8 @@
+module Prelude
+  ( module X
+  ) where
+
+import           Diagrams.Prelude as X hiding ()
+import           Protolude        as X hiding (First, Last, Strict, conjugate,
+                                        from, getFirst, getLast, local, rotate,
+                                        shift, to, trace, uncons, unsnoc, zero)
