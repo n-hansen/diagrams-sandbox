d3635df37308946e0cac46f2a84ef217e82365e7
nick hansen <n-hansen@users.noreply.github.com>
2019-10-10 23:51:37 -0700

    fixup

--- src/LFrac.hs
+++ src/LFrac.hs
@@ -1,39 +1,31 @@
+module LFrac where
+
 import           Diagrams.Backend.Cairo
 import           Random
 
 ell = do
-  c <- randomHue 1 1
-  return $
-    fromOffsets [ unitY
-                , unitX
-                , 2 *^ unit_Y
-                , 2 *^ unit_X
-                , unitY
-                , unitX
-                ]
-    # strokeLoop
-    # centerXY
-    # fc c
+        c <- randomHue 1 1
+        return
+                $ fromOffsets
+                          [unitY, unitX, 2 *^ unit_Y, 2 *^ unit_X, unitY, unitX]
+                # strokeLoop
+                # centerXY
+                # fc c
 
 layTiles tile =
-  let tile' tr rot =
-        translate tr
-        . rotateBy rot
-        . scale 0.5
-        <$> tile
-      t1 = tile' zero 0
-      t2 = tile' (r2 (negate xShift, negate yShift)) 0.75
-      t3 = tile' (r2 (xShift, negate yShift)) 0
-      t4 = tile' (r2 (xShift, yShift)) 0.25
-  in
-    mconcat <$> sequence [t1,t2,t3,t4]
+        let tile' tr rot = translate tr . rotateBy rot . scale 0.5 <$> tile
+            t1 = tile' zero 0
+            t2 = tile' (r2 (negate xShift, negate yShift)) 0.75
+            t3 = tile' (r2 (xShift, negate yShift)) 0
+            t4 = tile' (r2 (xShift, yShift)) 0.25
+        in  mconcat <$> sequence [t1, t2, t3, t4]
 
-  where
-    xShift = 0.5
-    yShift = 0.5
+    where
+        xShift = 0.5
+        yShift = 0.5
 
 main = renderCairo out sz =<< d
-  where
-    out = "out/lfrac.png"
-    sz = mkSizeSpec2D (Just 1000) Nothing
-    d = runRandom [1] . unsafeHead . drop 6 . iterate layTiles $ ell
+    where
+        out = "out/lfrac.png"
+        sz  = mkSizeSpec2D (Just 1000) Nothing
+        d   = runRandom [1] . unsafeHead . drop 6 . iterate layTiles $ ell
